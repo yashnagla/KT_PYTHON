31cd28d503f6dc238ab73aff5d2d9b537cd037f2
yashnagla <naglayash@gmail.com>
2025-01-04 10:43:50 +0530

    Practiced File Handling

--- file-io.py
+++ file-io.py
@@ -1,31 +1,31 @@
-# # Reading File
-# file = open("demofile.txt", "r")
-# print(file.read())
-# file.close()
+# Reading File
+file = open("demofile.txt", "r")
+print(file.read())
+file.close()
 
-# file = open("demofile.txt", "r")
-# print(file.read(5))
-# file.close()
+file = open("demofile.txt", "r")
+print(file.read(5))
+file.close()
 
-# file = open("demofile.txt", "r")
-# print(file.readline())
-# file.close()
+file = open("demofile.txt", "r")
+print(file.readline())
+file.close()
 
-# file = open("demofile.txt", "r")
-# for x in file:
-#     print(x, end="**")
+file = open("demofile.txt", "r")
+for x in file:
+    print(x, end="**")
 
 
 
-# # Writing in a file
-# # Using "a" = Append
-# file = open("demofile_to_write.txt", "w")
-# file.write("'w' removes the old data and then add new data.")
-# file.close()
+# Writing in a file
+# Using "a" = Append
+file = open("demofile_to_write.txt", "w")
+file.write("'w' removes the old data and then add new data.")
+file.close()
 
-# file = open("demofile_to_write.txt", "a")
-# file.write("'a' add the new data after old data.")
-# file.close()
+file = open("demofile_to_write.txt", "a")
+file.write("'a' add the new data after old data.")
+file.close()
 
 # Delete File
 import os
